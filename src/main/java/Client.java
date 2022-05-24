9c7712a8dc3bc1a6db81414cb1b4f18767534ace
SVKotov <syndareon@gmail.com>
2022-05-24 16:55:12 +0300

    Initial load

---
+++ src/main/java/Client.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+
+    public static void main(String[] args) {
+
+        String host = "127.0.0.1";
+        int port = 8081;
+
+        String message = "Aloha!";
+
+        try (
+                Socket clientSocket = new Socket(host, port);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+
+            System.out.println("now connected to " + clientSocket.getRemoteSocketAddress());
+
+            out.println(message);
+
+            String resp = in.readLine();
+
+            System.out.println("Server response: \n" + resp);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
